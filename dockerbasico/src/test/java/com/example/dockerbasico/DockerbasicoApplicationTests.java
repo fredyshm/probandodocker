fb7cf015c5ead10025c37413ae3ff34e48d1ded3
fredyshm <1412424@utp.edu.pe>
2020-10-16 15:09:50 -0500

    subida de docker

---
+++ dockerbasico/src/test/java/com/example/dockerbasico/DockerbasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerbasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerbasicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
